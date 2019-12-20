d8f2348ab7ec267c53e8e0c11074d36a022396e7
begueradj <begueradj.bill@gmail.com>
2019-12-20 09:06:34 +0200

    Add FalvioCopes example

---
+++ components/PageComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <main>
+      <slot v-bind:myName="myName" />
+    </main>
+    <p> Page component </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageComponent',
+  data () {
+    return {
+      myName: 'Begueradj'
+    }
+  }
+}
+</script>
